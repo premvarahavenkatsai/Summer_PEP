5ed5298bb47b1e9d4328f088ec9f560630262552
prem sai <your_email@example.com>
2026-07-01 15:18:06 +0530

    commit

---
+++ 2D_Matrix/SumOfRow.java
@@ -0,0 +1,29 @@
+public class SumOfRow{
+ public static void main(String[] args){
+      int[][] matrix = {
+         {6,11,4,10},
+         {13,1,15,2},
+         {8,12,5,14},
+         {3,16,9,7}
+      };
+      int index = 0;
+      int top = 0;
+      int left = 0;
+      int[] arr = new int[(matrix.length * matrix[0].length) - 2];
+      int bottom = matrix.length - 1;
+      int right = matrix[0].length - 1;
+      while(left <= right){
+         for(int i=top;i<bottom;i++){
+            arr[index++] = matrix[i][left];
+         } 
+         left++;
+         for(int i=bottom;i>=top;i--){
+            arr[index++] = matrix[i][left];
+         }
+         left++;
+      }
+      for(int i=0;i<arr.length;i++){
+         System.out.print(arr[i]+" ");
+      }
+   }
+}
